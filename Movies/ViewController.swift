2096a14bd75471e5e0c3e780cdce78d1f40ccb6a
Preeten Dali <preetendali@gmail.com>
2024-11-21 19:42:40 +0530

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Preeten Dali on 21/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
